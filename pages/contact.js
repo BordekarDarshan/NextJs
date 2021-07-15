279256b48d2e735d7fe254e6d5f42a4518483805
darshan bordekar <darshanbordekar1819@gmail.com>
2021-07-15 11:56:21 +0530

    [ Darshan ] | Project Time

--- pages/contact.js
+++
@@ -1,9 +0,0 @@
-import styles from "../styles/Home.module.css";
-
-export default function contact() {
-  return (
-    <div className={styles.container}>
-      <h1>This is Static Route</h1>
-    </div>
-  );
-}
--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,9 @@
-import { useRouter } from "next/router";
-import Link from "next/link";
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
-  let router = useRouter();
   return (
     <div className={styles.container}>
-      <h1 onClick={() => router.push("/about/statistics")}>Home</h1>
-      <Link href="/clients">Our Clients</Link>
+      <h1>Home</h1>
     </div>
   );
 }
